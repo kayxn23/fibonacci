a2d528102f3f85e68f3fa2a81773ce268efc6e05
Kay <kayxn23@gmail.com>
2018-10-28 15:56:03 -0700

    3 failing tests

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,20 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-  raise NotImplementedError
+  raise ArgumentError if n.nil?
+  raise ArgumentError if n < 0
+  return 0 if n == 0
+
+  x = 0
+  y = 1
+  z = x + y # z starts with 1
+
+  while z < n
+    x = y
+    y = z
+    z = x + y
+  end
+  return z
 end
+
+# 0 1 1 2 3 5 8 13 21 34 55 89 144
